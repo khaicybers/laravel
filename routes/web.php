75af8c575ee705a6704a0169a4eef60b3ccc2199
khaicybers <khaicybers@gmail.com>
2024-09-05 11:08:51 +0700

    update1

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,15 @@ Route::get('upload', [HomeController::class, 'upload'])->name('home.upload');
 // route này validate dữ liệu khi submit form
 Route::post('upload', [HomeController::class, 'handle_upload']);
 //route category
-Route::get('category',[CategoryController::class,'index'])->name('category.index');
+Route::get('Category', function () {
+    return view('category.index');
+});
+Route::get('category','CategoryController@index')->name('category.index');
+
 // GET categorry/create để hiển thị form nhập dữ liệu
-Route::get('category/create',[CategoryController::class,'create'])->name('category.create');
+Route::get('category/create','CategoryController@create')->name('category.create');
 // POST categorry để nhận dữ liệu khi submit form
-Route::post('category',[CategoryController::class,'store'])->name('category.store');
+Route::post('category','CategoryController@store')->name('category.store');
 
-// Route::get('category',[CategoryController::class,'delete'])->name('category.delete');
-Route::delete('category/{id}',[CategoryController::class,'delete'])->name('category.delete');
 
-Route::get('category/{id}/edit',[CategoryController::class,'edit'])->name('category.edit');
-Route::put('category/{id}',[CategoryController::class,'update'])->name('category.update');
 
-Route::get('', [HomeController::class, 'index'])->name('home.index');
